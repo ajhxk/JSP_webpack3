19a5822369d61b428b0e8c5a0079142e25657aba
ajhxk <ajhxk@outlook.com>
2020-04-26 10:29:09 +0800

    master

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# JSP_webpack3
+## 让你快速上手一个demo
+
+### 1.配置文件webpack.config.js
+### 2.entry选项（入口配置）
+### 3.output选项（出口配置）
+### 4.多入口、多出口配置
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: {
+        
+    },
+    output: {},
+    // 处理文件 css 图片等
+    module: {},
+    plugins: [],
+    desServer: {},
+}
