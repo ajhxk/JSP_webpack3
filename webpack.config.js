94a25b12e90f4dd7cd417c55ba9625b0085f4975
ajhxk <ajhxk@outlook.com>
2020-04-26 11:11:43 +0800

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,10 @@ module.exports = {
     // 处理文件 css 图片等
     module: {},
     plugins: [],
-    devServer: {},
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        host: '192.168.0.114',
+        compress: true,
+        port: 8080
+    },
 }
